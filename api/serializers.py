205bf51bf06a9395f3d64c18187dee848d702aa0
Hakim <hakimghaziali@gmail.com>
2022-09-18 16:31:23 -0400

    complete get request in views

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,24 @@
+from dataclasses import field
 from rest_framework import serializers
 from reminder.models import Reminder , Message 
 
 
 
+class MessageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Message 
+        fields = ('heading','reminder',)
+
+
+
 class ReminderSerializer(serializers.ModelSerializer):
+
+    t_reminder = MessageSerializer(many=True, required=False)
 
     class Meta:
         model = Reminder
         fields = '__all__'
+
+
+
--- api/views.py
+++ api/views.py
@@ -1,12 +1,36 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework.views import APIView
 from reminder.models import Reminder , Message
 # Create your views here.
 from .serializers import ReminderSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
+import datetime
 
-ReminderSerializer
 
-class ReminderApi(generics.ListAPIView):
-    
-    queryset = Reminder.objects.all()
-    serializer_class = ReminderSerializer 
+class ReminderApi(APIView):
+
+    def get(self, request):
+
+        try:
+            
+            reminder = Reminder.objects.get(date_message=datetime.date.today())
+            data = ReminderSerializer(reminder).data
+            return Response(data)
+
+        except:
+            
+            data = {
+                "message" : 'today you dont have any reminder'
+            }
+            return JsonResponse(data)
+
+
+    def post(self, request, format=None):
+        serializer = ReminderSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
