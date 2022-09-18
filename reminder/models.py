833cbe0c369b1aa7c583827e6a4c1947d80ee50e
Hakim <hakimghaziali@gmail.com>
2022-09-18 07:31:53 -0400

    add models to app

--- reminder/models.py
+++ reminder/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Reminder(models.Model):
+
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    date_reminder = models.DateField()
+    date_create = models.DateTimeField(auto_now=True)
+    date_message = models.DateField()
+
+
+
+
+class message(models.Model):
+
+    reminder = models.ForeignKey(Reminder ,on_delete=models.CASCADE , related_name='t_reminder' )
+    heading = models.CharField(max_length=100)
+    
