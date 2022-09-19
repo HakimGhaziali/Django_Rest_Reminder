fd6071f2df4ab314ac335c192d31a16dcc670d48
Hakim <hakimghaziali@gmail.com>
2022-09-19 04:51:42 -0400

    end task

--- api/views.py
+++ api/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from reminder.models import Reminder , Message
-# Create your views here.
 from .serializers import ReminderSerializer , MessageSerializer
 from rest_framework.response import Response
 from rest_framework import status
